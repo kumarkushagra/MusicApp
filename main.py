96a7046d9c3e223538f3ce9753ba6b3252269572
h-sharda <harshitsharda0@gmail.com>
2024-12-10 00:28:10 +0530

    adding

--- main.py
+++ main.py
@@ -10,23 +10,27 @@ LIBRARY = os.path.join(os.getcwd(), "library")
 SONG_LINK = ""
 
 # main Code
-attempts = 0
-donwloaded = False
+def main():
+    attempts = 0
+    donwloaded = False
 
-while attempts < MAX_ATTEMPTS:
-    print(f"Attempt {attempts + 1} of {MAX_ATTEMPTS} to download from fast site.")
-    try:
-        fast.main(ROOT_DIR, LIBRARY, SONG_LINK)
-        print("Download successful from the fast site.")
-        donwloaded = True
-        break
-    except Exception as e:
-        print(f"Fast site download failed: {e}")
-        
-    attempts += 1
-    time.sleep(1)
+    while attempts < MAX_ATTEMPTS:
+        print(f"Attempt {attempts + 1} of {MAX_ATTEMPTS} to download from fast site.")
+        try:
+            fast.main(ROOT_DIR, LIBRARY, SONG_LINK)
+            print("Download successful from the fast site.")
+            donwloaded = True
+            break
+        except Exception as e:
+            print(f"Fast site download failed: {e}")
+            
+        attempts += 1
+        time.sleep(1)
 
-if not donwloaded:
-    print(f"Fast site download failed after {MAX_ATTEMPTS} attempts. Trying the robust site.")
-    robust.main(ROOT_DIR, LIBRARY, SONG_LINK)
-    print("Download successful from the robust site.")
+    if not donwloaded:
+        print(f"Fast site download failed after {MAX_ATTEMPTS} attempts. Trying the robust site.")
+        robust.main(ROOT_DIR, LIBRARY, SONG_LINK)
+        print("Download successful from the robust site.")
+
+if __name__ == "__main__":
+    main()
