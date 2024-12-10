2ac03f323af704a2f51eb0decd7cddde0b2215f2
h-sharda <harshitsharda0@gmail.com>
2024-12-10 20:01:16 +0530

    neww

--- downloadSongs/functions.py
+++ downloadSongs/functions.py
@@ -26,9 +26,9 @@ def move_file_to_library(source, target):
 def setup_driver(download_dir):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument(f'--download-default-directory={download_dir}')
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-application-cache')
 
     prefs = {
