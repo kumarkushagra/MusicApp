f2b651b6aad6d9fb142be52f34d3bb57ae6964f5
vector <143745200+h-sharda@users.noreply.github.com>
2024-12-10 20:46:30 +0530

    Update download.py

--- downloadSongs/download.py
+++ downloadSongs/download.py
@@ -10,19 +10,19 @@ LIBRARY = os.path.join(os.getcwd(), "library")
 
 # main code
 def main(SONG_LINK):
-    attempts = 0
+    # attempts = 0
 
-    while attempts < MAX_ATTEMPTS:
-        print(f"Attempt {attempts + 1} of {MAX_ATTEMPTS} to download from fast site.")
-        try:
-            fast.main(ROOT_DIR, LIBRARY, SONG_LINK)
-            print("Download successful from the fast site.")
-            return True
-        except Exception as e:
-            print(f"Fast site download failed: {e}")
+    # while attempts < MAX_ATTEMPTS:
+    #     print(f"Attempt {attempts + 1} of {MAX_ATTEMPTS} to download from fast site.")
+    #     try:
+    #         fast.main(ROOT_DIR, LIBRARY, SONG_LINK)
+    #         print("Download successful from the fast site.")
+    #         return True
+    #     except Exception as e:
+    #         print(f"Fast site download failed: {e}")
             
-        attempts += 1
-        time.sleep(1)
+    #     attempts += 1
+    #     time.sleep(1)
 
     try:
         print(f"Fast site download failed after {MAX_ATTEMPTS} attempts. Trying the robust site.")
